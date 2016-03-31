8beb03c8f3a0e30ff363a535c361a4615edf82c2
LiuZHao <comst@comstdemacbook-pro.local>
2016-03-31 10:06:24 +0800

    init

---
+++ LZColorDetector/LZColorDetector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZColorDetector
+//
+//  Created by comst on 16/3/31.
+//  Copyright © 2016年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
