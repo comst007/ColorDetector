d2dc1e439c9d04db0894edc948ad1ba6c9d185b9
LiuZHao <comst@comstdeMacBook-Pro.local>
2016-03-31 10:14:51 +0800

    new directory

---
+++ LZColorDetector/LZColorDetector/Classes/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZColorDetector
+//
+//  Created by comst on 16/3/31.
+//  Copyright © 2016年 com.comst1314. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
